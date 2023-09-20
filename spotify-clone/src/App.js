17c73e0f336c5c9bc72061a2a2e7fa7bc77fcdc8
allystor <alvarojoao17@gmail.com>
2023-09-19 22:54:18 -0300

    create components

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -3,18 +3,20 @@ import {Play, UsersThree, House, MagnifyingGlass, Books} from '@phosphor-icons/r
 export default function App() {
   return (
     
-    <div className='bg-zinc-900 text-zinc-50 h-screen'>
-      <div>
-        <aside>
+    <div className='bg-zinc-900 text-zinc-50 h-screen flex flex-col'>
+      <div className='flex flex-1 overflow-hidden'>
+        <aside className='w-96 bg-zinc-950 p-6'>
           Sidebar
         </aside>
-        <main>
+        <main className='flex-1 p-5 overflow-y-auto'>
           Main
         </main>
-        <footer>
-          footer
-        </footer>
       </div>
+      <footer className='bg-zinc-800 border-t border-zinc-700 flex p-6 items-center justify-center'>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </footer>
     </div>
 
   )
