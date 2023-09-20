43f3a6f9eccc0048cfe896a28a62e9a9c8c9b375
allystor <alvarojoao17@gmail.com>
2023-09-19 22:42:17 -0300

    start project

---
+++ spotify-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Play, UsersThree, House, MagnifyingGlass, Books} from '@phosphor-icons/react'
+export default function App() {
+  return (
+    
+    <div className='bg-zinc-900 text-zinc-50 h-screen'>
+      <div>
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+          Main
+        </main>
+        <footer>
+          footer
+        </footer>
+      </div>
+    </div>
+
+  )
+}
